cfa3884bf51b247e32a236c409585a48733ac883
Bartłomiej Kania <git@kania.ovh>
2024-10-29 12:19:00 +0100

    Added tests for Register page and created POMs and fixtures for Login

--- read-env-variables.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config({
-  path: `./e2e/config/.env.${process.env.TEST_ENV}`,
-});
-
-console.log(process.env.NEETO_CI_JOB_ID);
-console.log(process.env.TAG);
